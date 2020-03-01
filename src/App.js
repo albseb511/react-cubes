65784dbf372aad7d74ba6a0b1765d885d4935b78
albert <albseb511@github.com>
2020-03-01 19:45:47 +0530

    Updates: project files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Cube from "./components/Cube";
+import "./App.css";
 
 function App() {
+  const [cube, setCube] = useState({x:3, y:3, z:3})
+  const [size, setSize] = useState(1)
+  const [zoom, setZoom] = useState(10)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex">
+      <Cube cubeConfig={cube} cubeDimension={size} zoom={zoom}/>
+      <div className="container">
+        <div>X Y AND Z REPRESENTS THE NUMBER OF CUBES IN EACH DIRECTION</div>
+        <div>THE CUBE CHANGES COLOR ON EVERY TIME CHANGES OCCUR</div>
+        <div>
+          NUMBER OF CUBES ON EACH AXIS
+          <div>
+            <label>X: 
+              <input value={cube.x} onChange={(e)=>setCube({...cube, x:Number(e.target.value)})} type="number"/>
+            </label>
+          </div>
+          <div>
+            <label>Y: 
+              <input value={cube.y}  onChange={(e)=>setCube({...cube, y:Number(e.target.value)})} type="number"/>
+            </label>
+          </div>
+          <div>
+            <label>Z: 
+              <input value={cube.z}  onChange={(e)=>setCube({...cube, z:Number(e.target.value)})} type="number"/>
+            </label>
+          </div>
+        </div>
+        <div>
+          <label>
+            CUBE SIZE
+            <input value={size} onChange={e=>setSize(Number(e.target.value))} type="number"/>
+          </label>
+        </div>
+        <div>
+          <label>
+            ZOOM RATIO (DEFAULT 10)
+            <input value={zoom} onChange={e=>setZoom(Number(e.target.value))} type="number"/>
+          </label>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Cube.jsx
@@ -0,0 +1,93 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import * as Three from "three"
+import RandExp from 'randexp'
+
+class Cube extends Component {
+    static propTypes = {
+        prop: PropTypes
+    }
+    constructor(props){
+        super(props)
+        this.state = {
+            cube: [],
+            positions: [],
+        }
+    }
+    initialise = () =>{
+        let positions = []
+        const {cubeConfig} = this.props
+        const {x,y,z} = cubeConfig
+        for(let i = 0; i<x; i++){
+            for(let j = 0; j<y; j++){
+                for(let k = 0; k<z; k++){
+                    positions.push([i,j,k])
+                }
+            }
+        }
+        this.setState({
+            cube: new Array(positions.length).fill(0),
+            positions
+        },this.render3D)
+    }
+    render3D = async()=>{
+        let { cubeDimension:dim, cubeSpacing:gap, zoom } = this.props
+        let {positions} = this.state
+        this.scene = new Three.Scene()
+        this.camera = new Three.PerspectiveCamera( 90, (window.innerWidth/2)/window.innerHeight, 0.1, 1000)
+        this.renderer = new Three.WebGLRenderer( {antialias: true})
+        this.renderer.setSize(window.innerWidth/2, window.innerHeight)
+        if(this.elem.childElementCount)
+            this.elem.firstChild.remove()
+        this.elem.appendChild(this.renderer.domElement)
+        this.cubes = new Three.Group()
+        for(let i=0; i<positions.length; i++){
+            let geometry = new Three.BoxGeometry(dim,dim,dim)
+            let material = new Three.MeshBasicMaterial( {color: Number(new RandExp(/^0x[0-8a-f]{6}$/).gen())})
+            let cube = new Three.Mesh(geometry,material)
+            cube.position.set( ...this.state.positions[i].map(a=>(a*dim)) );
+            this.cubes.add(cube)
+        }
+        
+        this.scene.add(this.cubes)
+        this.camera.position.x = zoom;
+        this.camera.position.y = zoom;
+        this.camera.position.z = zoom;
+        this.camera.lookAt(this.scene.position)
+        this.renderer.render(this.scene, this.camera);
+    }
+    componentDidMount(){
+        this.initialise()
+    }
+    componentDidUpdate(prevProps){
+        let hasDimensionChanged = prevProps.cubeDimension!==this.props.cubeDimension
+        let hasXChanged = prevProps.cubeConfig.x !== this.props.cubeConfig.x
+        let hasYChanged = prevProps.cubeConfig.y !== this.props.cubeConfig.y
+        let hasZChanged = prevProps.cubeConfig.z !== this.props.cubeConfig.z
+        let hasZoomChanged = prevProps.zoom !== this.props.zoom
+        if(hasDimensionChanged || hasXChanged || hasYChanged || hasZChanged || hasZoomChanged)
+            this.initialise()
+    }
+    render() {
+        return (
+            <div ref={ele=>{this.elem = ele}}>
+            </div>
+        )
+    }
+}
+
+Cube.propTypes = {
+    cubeConfig: PropTypes.object,
+    cubeDimension: PropTypes.number,
+    cubeSpacing: PropTypes.number,
+    zoom: PropTypes.number
+}
+
+Cube.defaultProps = {
+    cubeConfig: {x:3, y:3, z:3},
+    cubeDimension: 1,
+    cubeGap: 0,
+    zoom: 10
+}
+
+export default Cube
